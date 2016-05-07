b48ba76cfac62e43535e3d5e1256fe2b323b8d42
arturmalinowski <artur@wow.com>
2016-05-07 12:14:24 +0100

    Artur - Fixing Jordan

--- jordan-core/src/main/java/net/wazim/jordan/properties/JordanProductionProperties.java
+++ jordan-core/src/main/java/net/wazim/jordan/properties/JordanProductionProperties.java
@@ -6,8 +6,8 @@ import static java.net.URI.create;
 
 public class JordanProductionProperties implements JordanProperties {
 
-    public static final URI AMAZON_BASE_URL = create("http://www.amazon.co.uk/");
-    public static final URI AMAZON_QUERY_URL = create("http://www.amazon.co.uk/s/ref=sr_adv_d?__mk_en_GB=%C5M%C5Z%D5%D1&search-alias=dvd&unfiltered=1&field-keywords=&field-title=&field-actor=&field-director=&field-label=&field-ean=&field-price=0-850&field-intended_use_browse-bin=&field-binding_browse-bin=383380011&node=&field-theme_browse-bin=&field-dvd-region=&emi=&sort=&Adv-Srch-DVD-Submit.x=61&Adv-Srch-DVD-Submit.y=17");
+    public static final URI AMAZON_BASE_URL = create("https://www.amazon.co.uk/");
+    public static final URI AMAZON_QUERY_URL = create("https://www.amazon.co.uk/s/ref=sr_adv_d?__mk_en_GB=%C5M%C5Z%D5%D1&search-alias=dvd&unfiltered=1&field-keywords=&field-title=&field-actor=&field-director=&field-label=&field-ean=&field-price=0-850&field-intended_use_browse-bin=&field-binding_browse-bin=383380011&node=&field-theme_browse-bin=&field-dvd-region=&emi=&sort=&Adv-Srch-DVD-Submit.x=61&Adv-Srch-DVD-Submit.y=17");
 
     @Override
     public URI getRequestUrl() {
