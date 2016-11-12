65d56d3990be66325595746bc438991d88b50d9e
Jonathan Sharifi <jonathansharifi@gmail.com>
2016-11-12 17:41:29 +0000

    Jon: Spoofing headers - not using proxy

--- jordan-core/src/main/java/net/wazim/jordan/client/JordanHttpClient.java
+++ jordan-core/src/main/java/net/wazim/jordan/client/JordanHttpClient.java
@@ -1,15 +1,18 @@
 package net.wazim.jordan.client;
 
+import org.apache.http.impl.client.HttpClientBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.client.ClientHttpResponse;
-import org.springframework.http.client.SimpleClientHttpRequestFactory;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.DefaultResponseErrorHandler;
 import org.springframework.web.client.RestTemplate;
 
 import java.io.IOException;
-import java.net.Proxy;
 import java.net.URI;
 
 public class JordanHttpClient {
@@ -24,12 +27,19 @@ public class JordanHttpClient {
     }
 
     public JordanHttpResponse getRequest(URI requestUrl) {
-        Proxy proxy = ProxyProvider.getProxy();
-        logger.info("Using " + proxy.toString() + " for " + requestUrl.toASCIIString());
-        SimpleClientHttpRequestFactory requestFactory = new SimpleClientHttpRequestFactory();
-        requestFactory.setProxy(proxy);
-        webClient.setRequestFactory(requestFactory);
-        ResponseEntity<String> response = webClient.getForEntity(requestUrl.toASCIIString(), String.class);
+//        HttpHost proxy = ProxyProvider.getProxy();
+//        logger.info("Using " + proxy.toString() + " for " + requestUrl.toASCIIString());
+        HttpComponentsClientHttpRequestFactory clientHttpRequestFactory = new HttpComponentsClientHttpRequestFactory(
+                HttpClientBuilder.create().build()
+        );
+        webClient.setRequestFactory(clientHttpRequestFactory);
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.set("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8");
+        httpHeaders.set("Accept-Encoding", "gzip, deflate, sdch, br");
+        httpHeaders.set("Accept-Language", "en-US,en;q=0.8");
+        httpHeaders.set("User-agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36");
+        HttpEntity<String> requestEntity = new HttpEntity<>(httpHeaders);
+        ResponseEntity<String> response = webClient.exchange(requestUrl.toASCIIString(), HttpMethod.GET, requestEntity, String.class);
 
         return new JordanHttpResponse(response.getStatusCode().value(), response.getBody());
     }
--- jordan-core/src/main/java/net/wazim/jordan/client/ProxyProvider.java
+++ jordan-core/src/main/java/net/wazim/jordan/client/ProxyProvider.java
@@ -2,39 +2,37 @@ package net.wazim.jordan.client;
 
 import com.google.common.cache.CacheLoader;
 import com.google.common.cache.LoadingCache;
+import org.apache.http.HttpHost;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
-import java.net.InetSocketAddress;
-import java.net.Proxy;
 import java.util.concurrent.ExecutionException;
 
 import static com.google.common.cache.CacheBuilder.newBuilder;
-import static java.net.Proxy.Type.HTTP;
 import static java.util.concurrent.TimeUnit.MINUTES;
 
 class ProxyProvider {
 
-    private static final LoadingCache<String, Proxy> activeProxy = newBuilder()
+    private static final LoadingCache<String, HttpHost> activeProxy = newBuilder()
             .expireAfterAccess(5, MINUTES)
             .build(
-                    new CacheLoader<String, Proxy>() {
-                        public Proxy load(String key) throws JSONException {
+                    new CacheLoader<String, HttpHost>() {
+                        public HttpHost load(String key) throws JSONException {
                             RestTemplate restTemplate = new RestTemplate();
-                            ResponseEntity<String> response = restTemplate.getForEntity("http://gimmeproxy.com/api/getProxy", String.class);
+                            ResponseEntity<String> response = restTemplate.getForEntity("http://gimmeproxy.com/api/getProxy?supportsHttps=true&maxCheckPeriod=1000?cookies=true?user-agent=true", String.class);
                             JSONObject jsonObject = new JSONObject(response.getBody());
-                            return new Proxy(HTTP,
-                                    new InetSocketAddress(
-                                            jsonObject.getString("ip"),
-                                            jsonObject.getInt("port")
-                                    ));
+                            return new HttpHost(
+                                    jsonObject.getString("ip"),
+                                    jsonObject.getInt("port"),
+                                    "http"
+                            );
                         }
                     }
             );
 
-    static Proxy getProxy() {
+    static HttpHost getProxy() {
         try {
             return activeProxy.get("");
         } catch (ExecutionException e) {
